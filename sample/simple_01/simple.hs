39b819b004284ac5ef5d419805a6a0b38d8a4abd
John Kenyon <JLKenyon@gmail.com>
2010-12-26 09:04:37 +0100

    Expaneded BuildSystem to contain top level targets, added Dot generation for debugging

--- sample/simple_01/simple.hs
+++ sample/simple_01/simple.hs
@@ -25,3 +25,14 @@ main = do
            cpp "src/bar.cpp"
          exe "simple_foo" $ do
            cpp "src/foo.cpp"
+
+  putStrLn "---------------------------------------"
+  putStrLn "Dotted complex project:"
+  putStrLn $ genDot $ project "HelloWorld" $ do
+         output_dir "bin"
+         exe "simple_01" $ do
+           cpp "src/main.cpp"
+           cpp "src/bar.cpp"
+         exe "simple_foo" $ do
+           cpp "src/foo.cpp"
+
--- src/Hammock.hs
+++ src/Hammock.hs
@@ -28,15 +28,17 @@
 module Hammock where
 
 import System.FilePath.Posix
+import Data.List
+import Data.Set (difference, fromList, elems)
     
 version = "0.0.1"
 
-data BuildConfig = CppConfig { _output_dir :: FilePath
-                             , _include_dirs :: [FilePath]
-                             , _link_dirs :: [FilePath]
-                             , _link_libs :: [String]
-                             , _defines :: [(String, String)]
-                             }
+data BuildConfig = BuildConfig { _output_dir :: FilePath
+                               , _include_dirs :: [FilePath]
+                               , _link_dirs :: [FilePath]
+                               , _link_libs :: [String]
+                               , _defines :: [(String, String)]
+                               }
                    deriving(Eq, Show)
                            
 data Action = Action { _input  :: [FilePath]
@@ -47,6 +49,7 @@ data Action = Action { _input  :: [FilePath]
 
 data BuildSystem a = BuildSystem { actionList :: [Action]
                                  , __config :: BuildConfig
+                                 , top :: [String]
                                  , value :: a
                                  }
                      deriving(Eq, Show)
@@ -54,17 +57,23 @@ data BuildSystem a = BuildSystem { actionList :: [Action]
 -- --------------------------------------------------------
 
 mergeConfig :: BuildConfig -> BuildConfig -> BuildConfig
-mergeConfig lConfig rConfig = undefined
+mergeConfig lConfig rConfig = BuildConfig {
+                                _output_dir   = _output_dir rConfig
+                              , _include_dirs = _include_dirs lConfig ++ _include_dirs rConfig
+                              , _link_dirs    = _link_dirs    lConfig ++ _link_dirs    rConfig
+                              , _link_libs    = _link_libs    lConfig ++ _link_libs    rConfig
+                              , _defines      = _defines      lConfig ++ _defines      rConfig
+                              }
 
 instance Monad BuildSystem where
-    return a = BuildSystem [] emptyConfig a
-    m >>= k  = let (BuildSystem p_list p_config p_val) = m
-                   (BuildSystem n_list n_config n_val) = k p_val
-               in BuildSystem (p_list ++ n_list) (mergeConfig p_config n_config) n_val
+    return a = BuildSystem [] emptyConfig [] a
+    m >>= k  = let (BuildSystem p_list p_config p_top p_val) = m
+                   (BuildSystem n_list n_config n_top n_val) = k p_val
+               in BuildSystem (p_list ++ n_list) (mergeConfig p_config n_config) (p_top ++ n_top) n_val
 
 -- --------------------------------------------------------
 
-emptyConfig = CppConfig { _output_dir   = "" :: FilePath
+emptyConfig = BuildConfig { _output_dir   = "" :: FilePath
                         , _include_dirs = [] :: [FilePath]
                         , _link_dirs    = [] :: [FilePath]
                         , _link_libs    = [] :: [String]
@@ -73,20 +82,49 @@ emptyConfig = CppConfig { _output_dir   = "" :: FilePath
 
 -- --------------------------------------------------------
 
+genActionDot :: Action -> [String]
+genActionDot (Action input output _) = ["  " ++ show x ++ " -> " ++ show y ++ "\n" | x <- input, y <- output]
+
+genDot :: BuildSystem a -> String
+genDot build = header ++ content ++ footer
+    where header = "digraph G {\n"
+          footer = "}\n"
+          content = concat $ concat $ map genActionDot alist
+          -- content = concat $ concat $ map _input alist
+          -- content = intercalate ("\n"::FilePath) ((map _input alist)::[FilePath])
+          (BuildSystem alist _ _ _) = build
+
+-- --------------------------------------------------------
+
 cpp :: String -> BuildSystem String
-cpp fname = BuildSystem [action] emptyConfig fname
-    where action = Action [fname] [(convert_to_obj fname)] emptyConfig
+cpp fname = BuildSystem [action] emptyConfig [objname] fname
+    where action = Action [fname] [objname] emptyConfig
+          objname = convert_to_obj fname
           convert_to_obj :: String -> String
           convert_to_obj = flip replaceExtension ".o"
 
 project :: String -> BuildSystem String -> BuildSystem String
-project pname build = (return pname) :: (BuildSystem String)
+project pname build = BuildSystem (new_action : sub_alist) sub_config [pname] pname
+    where (BuildSystem sub_alist sub_config sub_top _) = build
+          new_action = Action sub_top [pname] sub_config
 
 
 exe :: String -> BuildSystem String -> BuildSystem String
-exe fname build = (return fname) :: (BuildSystem String)
-
+exe fname build = BuildSystem (new_action : sub_alist) sub_config [fname] fname
+    where (BuildSystem sub_alist sub_config sub_top _) = build
+          -- This is where I need to do the graph search to determine
+          -- which are "un-directed" actions... for now I will
+          -- fudge it...
+          -- new_action = Action (concat $ map _output sub_alist) [fname] sub_config
+          new_action = Action sub_top [fname] sub_config
+    
 
 output_dir :: String -> BuildSystem ()
-output_dir dir_name = return () :: (BuildSystem ())
+output_dir out_dir =  BuildSystem [] config [] ()
+    where config = BuildConfig { _output_dir = out_dir
+                               , _include_dirs = []
+                               , _link_dirs = []
+                               , _link_libs = []
+                               , _defines = []
+                               }
 
